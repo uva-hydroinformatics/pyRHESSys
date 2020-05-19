72d479b8699e5757953d0d9c8776b7172cdf4241
DavidChoi76 <yc5ef@virginia.edu>
2020-05-19 04:06:55 -0400

    add code to read r and shell script

--- pyrhessys/simulation.py
+++ pyrhessys/simulation.py
@@ -24,7 +24,7 @@ SHELL_R = pkg_resources.resource_listdir(
         __name__, 'shell_R/')
 code =[]
 for file in SHELL_R:
-    with open("shell_R/"+file, 'r') as f:
+    with open(file, 'r') as f:
         code.append(f.read())
 
 class Simulation():
