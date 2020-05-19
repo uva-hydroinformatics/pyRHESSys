9d1ec3233c088471c9147e6f7ce00d1862130c70
DavidChoi76 <yc5ef@virginia.edu>
2020-05-19 04:40:14 -0400

    edit read_code.py

--- pyrhessys/read_code.py
+++ pyrhessys/read_code.py
@@ -5,4 +5,4 @@ import pkg_resources
 DELINEATION_1 = pkg_resources.resource_filename(
         __name__, 'shell_R/grass_delineation_1.sh')
 with open(DELINEATION_1, 'r') as f:
-    DELINEATION_1 = json.load(f)
+    DELINEATION_1 = f.read()
