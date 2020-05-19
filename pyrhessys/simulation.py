53c956e2cc84234d0fdb9b3ba551beb7910a62dd
DavidChoi76 <yc5ef@virginia.edu>
2020-05-19 04:25:53 -0400

    add read_code module

--- pyrhessys/simulation.py
+++ pyrhessys/simulation.py
@@ -11,7 +11,7 @@ from typing import List
 
 from .plotting import Plotting
 from .input_configure import TimeSeries
-import read_code as code
+import .read_code as code
 
 PARAMETER = pkg_resources.resource_filename(
         __name__, 'meta/parameter_meta.json')
