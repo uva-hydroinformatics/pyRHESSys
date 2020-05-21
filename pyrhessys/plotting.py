1749eb7f16fc301852cf73763e99c0bc754f7b9b
Young-Don Choi <choiyd1115@gmail.com>
2020-05-21 15:24:25 -0400

    add annotation to avoid indentation error

--- pyrhessys/plotting.py
+++ pyrhessys/plotting.py
@@ -96,7 +96,7 @@ class Plotting:
 		fig.legend(labels=name_list, bbox_to_anchor=(0.82, 0.82))
 		plt.show()
 
-	def daily_patch_map(netcdf, variable, timestep):
+	#def daily_patch_map(netcdf, variable, timestep):
 	    """
     	Create a map for a variable at a given timestep
     	Warning: This function is state dependent! 
@@ -110,37 +110,37 @@ class Plotting:
     	"""
     	# Convert the data from an xarray dataset to a pandas dataframe 
     	# This is necessary for geoviews to be able to plot it
-		out_df = netcdf[variable].sel(time=timestep).to_dataframe()
+	#	out_df = netcdf[variable].sel(time=timestep).to_dataframe()
 		# Make sure we have some metadata to join with the shapefile
-		out_df['gridcode'] = shapes_df['gridcode'].values
+	#	out_df['gridcode'] = shapes_df['gridcode'].values
     	# Create the shape plot - some keys:
     	#  - shapes.records() provides the geometry from the shapefiles
     	#  - out_df provides the data
     	#  - value=var chooses which column of the dataframe to use as data for coloring
     	#  - index=['gridcode'] will provide a name on mouse hover
    		#  - on='gridcode' is the key to join the `shapes` with `out_df`
-		poly = gv.Shape.from_records(shapes.records(), out_df, value=variable, index=['gridcode'], on='gridcode')
+	#	poly = gv.Shape.from_records(shapes.records(), out_df, value=variable, index=['gridcode'], on='gridcode')
     	# Add some options to make things a bit nicer
     	#  - width=700, sets the width, as we expect
     	#  - cmap='plasma' sets the colormap to 'plasma'
     	#  - tools=['hover'] provides information on mouse hover over
     	#  - colorbar=True adds a colorbar
     	#  - alpha=0.7 adds a bit of transparency
-		poly = poly.opts(width=700, height=600, cmap='plasma', tools=['hover'], colorbar=True, alpha=0.7)
-		return poly	
+	#	poly = poly.opts(width=700, height=600, cmap='plasma', tools=['hover'], colorbar=True, alpha=0.7)
+	#	return poly	
 
-	def daily_patch_map_timeseries(netcdf, variable, start, stop, step, patch):
+	#def daily_patch_map_timeseries(netcdf, variable, start, stop, step, patch):
 		# Just as before, use the `var_map` function to build our map
-		poly = hv.HoloMap({t: daily_var_map(variable, t) for t in netcdf.time.values[start:stop:step]}, kdims=['timestep'])
+	#	poly = hv.HoloMap({t: daily_var_map(variable, t) for t in netcdf.time.values[start:stop:step]}, kdims=['timestep'])
     	# Vlines will give us an indicator of which time slice we are looking at
-		vlines = hv.HoloMap({t: hv.VLine(t) for t in netcdf.time.values[start:stop:step]}, kdims=['timestep'])
+	#	vlines = hv.HoloMap({t: hv.VLine(t) for t in netcdf.time.values[start:stop:step]}, kdims=['timestep'])
     	# This seems to make the plot appear more often - probably a holo/geoviews bug somewhere 
-		poly
+	#	poly
     
     	# Calculate min, max, and mean over the domain for each time
-		vmin = netcdf[variable].isel(time=slice(start, stop)).min(dim=patch).rolling(time=step).min().values
-		vmax = netcdf[variable].isel(time=slice(start, stop)).max(dim=patch).rolling(time=step).max().values
-		vmean = netcdf[variable].isel(time=slice(start, stop)).mean(patch).rolling(center=True, time=step).mean().dropna('time')
+	#	vmin = netcdf[variable].isel(time=slice(start, stop)).min(dim=patch).rolling(time=step).min().values
+	#	vmax = netcdf[variable].isel(time=slice(start, stop)).max(dim=patch).rolling(time=step).max().values
+	#	vmean = netcdf[variable].isel(time=slice(start, stop)).mean(patch).rolling(center=True, time=step).mean().dropna('time')
    
     	# Build the complete interactive plot. Layers are as follows
     	#  - gv.tile_sources.EsriTerrain- Add a background with topography
@@ -148,11 +148,11 @@ class Plotting:
     	#  - + hv.Area... - add the area plot to the right
     	#  - * hv.Curve... - overlay the mean curve onto the area plot
     	#  - * vlines... - overlay the timestep indicator
-		return (gv.tile_sources.EsriTerrain
-            	* poly
-            	+ (hv
-               		.Area((cw18.time[start:stop], vmin, vmax), vdims=['vmin', 'vmax'])
-               		.opts(alpha=0.5, color='gold', line_color=None)
-               		.redim.label(x='Date', vmin=var.capitalize()))
-            	* hv.Curve(vmean).opts(color='purple', alpha=0.8)
-            	* vlines.opts(alpha=0.4, color='red'))
+	#	return (gv.tile_sources.EsriTerrain
+    #        	* poly
+    #        	+ (hv
+    #          		.Area((cw18.time[start:stop], vmin, vmax), vdims=['vmin', 'vmax'])
+    #           		.opts(alpha=0.5, color='gold', line_color=None)
+    #           		.redim.label(x='Date', vmin=var.capitalize()))
+    #        	* hv.Curve(vmean).opts(color='purple', alpha=0.8)
+    #        	* vlines.opts(alpha=0.4, color='red'))
