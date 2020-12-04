ba4470c63257199161c7143940bc779e4f27a290
DavidChoi76 <yc5ef@virginia.edu>
2020-12-03 22:18:48 -0500

    set version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyRHESSys',
-      version='1.0.0',
+      version='0.0.1',
       description='an Object-Oriented Python wrapper for the RHESSys model',
       url='https://github.com/DavidChoi76/pyRHESSys.git',
       author='YoungDon Choi',
