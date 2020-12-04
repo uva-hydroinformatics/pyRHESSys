b29fac3db503c5d5b552669a24d123420610ad83
DavidChoi76 <yc5ef@virginia.edu>
2020-12-03 22:57:53 -0500

    update setup and version

--- pyrhessys/__init__.py
+++ pyrhessys/__init__.py
@@ -8,3 +8,7 @@ from .read_shellscript import *
 from .utils import *
 from .input_configure import *
 from .ostrich import Ostrich, OstrichParam
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
